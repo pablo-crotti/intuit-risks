080cedc9390e0c308bcb505e2166c0ace2e2f1a9
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-07-09 08:39:23 +0200

    Risk strategy visual

--- resources/js/Components/Risks/Components/RiskStrategy.vue
+++ resources/js/Components/Risks/Components/RiskStrategy.vue
@@ -14,8 +14,3 @@
     <span v-if="strategy == 'Veiller'" class="bg-blue block text-center min-w-max text-white px-2 py-1 rounded-md">{{ strategy }}</span>
     <span v-if="strategy == 'Accepter'" class="bg-green block text-center min-w-max text-white px-2 py-1 rounded-md">{{ strategy }}</span>
 </template>
-<!-- <style>
-* {
-    background-color: #129bbd;
-}
-</style> -->
