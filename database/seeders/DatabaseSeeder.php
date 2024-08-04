a8fb22160c48a0017c6b1f937fe1ac4df88dc8ca
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-08-04 15:31:39 +0200

    intuit-risks admin page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            // CategorySeeder::class,
-            // OrganizationTypeSeeder::class,
-            // RiskSeeder::class,
-            // CountrySeeder::class,
+            CategorySeeder::class,
+            OrganizationTypeSeeder::class,
+            RiskSeeder::class,
+            CountrySeeder::class,
             IntuitRisksAdminSeeder::class
 
         ]);
